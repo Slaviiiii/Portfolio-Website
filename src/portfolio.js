14a20ff67e3433543945d9fefc1fb1dab64fcfd4
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2023-08-18 13:24:39 +0300

    created portfolio file

---
+++ src/portfolio.js
@@ -0,0 +1,53 @@
+const about = {
+  name: "Slavi Karaslavov",
+  role: "Front-End dev",
+  social: {
+    github: "https://github.com/Slaviiiii",
+    instagram: "https://www.instagram.com/slavi_karaslavov_08/",
+    twitter: "https://twitter.com/slavikaraslavov",
+  },
+};
+
+const projects = [
+  {
+    name: "TechTrove",
+    description:
+      "TechTrove is an online shopping platform that offers a wide range of tech products, including electronics, gadgets, and accessories. It provides users with a seamless shopping experience, allowing them to browse through various products, add items to their cart, and securely complete their purchases.",
+    stack: ["Angular", "AngularFire", "RxJS", "Bootstrap", "Firebase"],
+    sourceCode: "https://github.com/Slaviiiii/TechTrove",
+    livePreview: "https://tech-trove-seven.vercel.app/home",
+  },
+  {
+    name: "Weather app",
+    description:
+      "An application which offers forecast and current weather of every city possible in the world.",
+    stack: ["React"],
+    sourceCode: "https://github.com/Slaviiiii/Weather-App",
+    livePreview: "https://weather-app-chi-lyart.vercel.app",
+  },
+  {
+    name: "Music project",
+    description:
+      "This is a full stack web application, which users use to create an account, login with it and make their music ideas come true. Users can like and comment on other music if they are logged in into their account. Owners can read their own creations in order to edit and delete them.",
+    stack: ["React", "React-Router-Dom"],
+    sourceCode: "https://github.com/Slaviiiii/react-music-project",
+    livePreview: "https://react-music-project-mu.vercel.app",
+  },
+];
+
+const skills = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Angular",
+  "Jest",
+  "Mocha",
+];
+
+const contact = {
+  email: "slavi@mail.com",
+};
+
+export { about, projects, skills, contact };
