04c62a0b48deabd5ff2ed8710cc404c62ac5097e
Slavi Ivanov Karaslavov <106228555+Slaviiiii@users.noreply.github.com>
2024-01-31 18:59:57 +0200

    Update Contact.js

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./Contact.css";
 
+import { contact } from "../../portfolio";
+
 const Contact = () => {
   return (
     <section className="contact-text" id="contact">
       <h1>Contact</h1>
       <p className="contact-p">You can contact me at </p>
-      <a href="mailto:slavi@gmail.com">slavi@gmail.com</a>
+      <a href=`mailto:${contact.email}`>{contact.email}</a>
     </section>
   );
 };
